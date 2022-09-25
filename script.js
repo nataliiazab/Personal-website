eb5dfa128f7c38f6c63a8d2decb65f4fc86e728a
nataliiazab <113237479+nataliiazab@users.noreply.github.com>
2022-09-25 17:35:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,68 +1,25 @@
-/* =================
-  TESTS, LOOK AT THESE
-  Reading tests will always help you discover your requirements.
-  You can make this window bigger. 
-   ===================
-*/
+var websites;
 
-const {
-  core: { test, expect, run },
-  prettify
-} = window.jestLite;
 
-/* =================
-  FIND ELEMENTS
-  These are all the elements we will look for.
-   ===================
-*/
-const getHeader = document.querySelectorAll("header"),
-  getH1 = document.querySelectorAll("h1"),
-  getSiteHeader = document.querySelectorAll(".c-site-header"),
-  getAria = document.querySelectorAll('nav[aria-label="Main Site Links."]'),
-  getMain = document.querySelectorAll("main"),
-  getFooter = document.querySelectorAll("footer"),
-  getSiteFooter = document.querySelectorAll(".c-site-footer"),
-  getIFrame = document.querySelectorAll("iframe"),
-  getImage = document.querySelectorAll("img"),
-  getWords = document.body.innerText;
+websites = ['LinkedIn', 'Netlify', 'Facebook'];
+websites.forEach((websites) => {
+  let element_list = document.getElementById('list');
+  let new_li = document.createElement('li');
+  let new_a = document.createElement('a');
+  if (websites == 'LinkedIn') {
+    new_a.setAttribute("href", 'https://www.linkedin.com/in/nataliia-zablotska/');
+    new_a.innerText = websites;
+  }
+  if (websites == 'Netlify') {
+    new_a.setAttribute("href", 'https://app.netlify.com/teams/nataliia-zab/overview');
+    new_a.innerText = websites;
+  }
+  if (websites == 'Facebook') {
+    new_a.setAttribute("href", 'https://www.facebook.com/nataliia.zab');
+    new_a.innerText = websites;
+  }
 
-/* =================
-   ASSERTIONS 
-   These are the things we check are true about your page.
-   Read and update your HTML to discover the requirements.
-   The tests will run every time you update your code.
-   ===================
-*/
-test("There is at least one header element", () => {
-  expect(getHeader.length).toBeGreaterThanOrEqual(1);
-});
-test("There is at least one h1", () => {
-  expect(getH1.length).toBeGreaterThanOrEqual(1);
-});
-test("There is only one header element with the class c-site-header", () => {
-  expect(getSiteHeader.length).toBe(1);
-});
-test("There is a nav element with an aria-label of Main Site Links.", () => {
-  expect(getAria.length).toBeGreaterThanOrEqual(1);
-});
-test("There is only one main element", () => {
-  expect(getMain.length).toBe(1);
-});
-test("There is at least one footer element", () => {
-  expect(getFooter.length).toBeGreaterThanOrEqual(1);
-});
-test("There is only one footer element with the class c-site-footer", () => {
-  expect(getSiteFooter.length).toBe(1);
-});
-test("There is embedded video", () => {
-  expect(getIFrame.length).toBeGreaterThanOrEqual(1);
-});
-test("There is at least one image", () => {
-  expect(getImage.length).toBeGreaterThanOrEqual(1);
-});
-test("There are at least 500 words on the page", () => {
-  expect(getWords.length).toBeGreaterThanOrEqual(500);
-});
+  new_li.appendChild(new_a);
 
-const console = document.getElementById("tests");
-prettify.toHTML(run(), console);
+  element_list.appendChild(new_li);
+});
